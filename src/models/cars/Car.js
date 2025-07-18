b854d7720c57415f86ad234f36213256727dfe7d
Ebenezer Sakyi <ebensakyi0@gmail.com>
2025-07-18 12:28:20 +0000

    fixes

--- src/models/cars/Car.js
+++ src/models/cars/Car.js
@@ -287,7 +287,7 @@ const carSchema = new mongoose.Schema({
     brakes: componentItemSchema,
     battery: componentItemSchema,
     engine: componentItemSchema,
-    gearBox: componentItemSchema,
+    transmission: componentItemSchema,
     suspension: componentItemSchema,
     body: componentItemSchema,
     interiorButtons: componentItemSchema,
